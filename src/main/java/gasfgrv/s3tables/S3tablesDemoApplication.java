382c0e891bd906edad93de68716e1989bd082bef
Gustavo A. Silva <gustavoalmeidasilva41@gmail.com>
2025-12-24 17:11:30 -0300

    Add ConfigurationPropertiesScan annotation to S3tablesDemoApplication

--- src/main/java/gasfgrv/s3tables/S3tablesDemoApplication.java
+++ src/main/java/gasfgrv/s3tables/S3tablesDemoApplication.java
@@ -2,12 +2,14 @@ package gasfgrv.s3tables;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class S3tablesDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(S3tablesDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(S3tablesDemoApplication.class, args);
+    }
 
 }
