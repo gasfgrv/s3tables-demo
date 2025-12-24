171d3886883de65e7d27376871114e4e728d1c78
Gustavo A. Silva <gustavoalmeidasilva41@gmail.com>
2025-12-24 17:12:10 -0300

    Refactor TesteController package structure and imports for clarity

--- src/main/java/gasfgrv/s3tables/application/ConsultaService.java
+++ src/main/java/gasfgrv/s3tables/application/ConsultaService.java
@@ -1,7 +1,8 @@
-package gasfgrv.s3tables.service;
+package gasfgrv.s3tables.application;
 
-import gasfgrv.s3tables.config.AwsProperties;
-import gasfgrv.s3tables.controller.dto.TableDto;
+import gasfgrv.s3tables.infrastructure.mapper.TableMapper;
+import gasfgrv.s3tables.infrastructure.prop.AwsProperties;
+import gasfgrv.s3tables.infrastructure.dto.TableDto;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3tables.S3TablesClient;
 import software.amazon.awssdk.services.s3tables.model.GetTableRequest;
@@ -12,20 +13,24 @@ public class ConsultaService {
 
     private final AwsProperties awsProperties;
     private final S3TablesClient s3TablesClient;
+    private final TableMapper mapper;
 
-    public ConsultaService(AwsProperties awsProperties, S3TablesClient s3TablesClient) {
+    public ConsultaService(AwsProperties awsProperties,
+                           S3TablesClient s3TablesClient,
+                           TableMapper mapper) {
         this.awsProperties = awsProperties;
         this.s3TablesClient = s3TablesClient;
+        this.mapper = mapper;
     }
 
     public TableDto consultarTabela() {
         GetTableRequest tableRequest = GetTableRequest.builder()
-                .tableArn(awsProperties.getS3TableArn())
+                .tableArn(awsProperties.s3TableArn())
                 .build();
 
         GetTableResponse tableResponse = s3TablesClient.getTable(tableRequest);
 
-        return TableDto.toDto(tableResponse.toString());
+        return mapper.toDto(tableResponse);
     }
 
 }
